c756d69938c3697da966819aa9b3d6ffa7462e4c
Cobeasta <stopyloknok@gmail.com>
2023-10-11 18:35:03 -0500

    things are working better

--- env-tool.sh
+++ env-tool.sh
@@ -4,12 +4,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 
-if [$1 == "alpine"]
-  then cd alpine
+if [[ "$1" == "alpine" ]]
+  then
+    cd alpine
 elif [$1 == "ubuntu"]
   then cd ubuntu
-
-docker-compose --env-file "config.env" up -d
+fi
+docker-compose up -d --build
 
 ssh builder@clion_docker -p 2222 && docker compose down
 
