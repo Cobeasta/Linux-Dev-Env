5050b767220e0c39336d95e6c0642e1369cf98d2
CKonkol <CKonkol@ippathways.com>
2023-11-07 11:28:59 -0600

    Add setup script for aliasing the scripts. Ideally scripts would be in bin directory or the user path.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ CALL_DIR=$(echo $pardir | tr '[:upper:]' '[:lower:]')
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 grep -qxF "alias alpine-dev-env=\"$SCRIPT_DIR/alpine/build-img.sh\"" ~/.bash_aliases || echo -e "\nalias alpine-dev-env=\"$SCRIPT_DIR/alpine/build-img.sh\"" >> ~/.bash_aliases
+
+grep -qxF "alias centos-dev-env=\"$SCRIPT_DIR/centos/build-img.sh\"" ~/.bash_aliases || echo -e "\nalias centos-dev-env=\"$SCRIPT_DIR/centos/build-img.sh\"" >> ~/.bash_aliases
