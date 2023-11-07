5f800f5adc0195bc7aeee60745061630fbf1fec5
CKonkol <CKonkol@ippathways.com>
2023-11-07 13:34:30 -0600

    automatically add centos .ssh/config entry, parameterize values in build script

--- centos/build-img.sh
+++ centos/build-img.sh
@@ -1,13 +1,23 @@
-PORTMOUNT=4444
+ENV_OS="centos"
+
+# Get caller's parent directory 
 pardir=${PWD##*/}
 pardir=${pardir:-/}
 CALL_DIR=$(echo $pardir | tr '[:upper:]' '[:lower:]')
 
+# Get directory of this script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t "dev-env-centos" --build-arg=$(id -u) -f "$SCRIPT_DIR/dockerfile.centos" $SCRIPT_DIR 
+# Get parameters
+source "$SCRIPT_DIR/config"
+echo "Building docker image for ${CENTOS_ENV_NAME}"
 
-echo "starting"
-docker run -dit --rm -p127.0.0.1:$PORTMOUNT:22 --mount type=bind,source=//$(pwd),destination=//"/home/builder/$CALL_DIR" --name="dev-env-centos" dev-env-centos
+docker build -t "${CENTOS_ENV_NAME}" --build-arg=$(id -u) -f "${SCRIPT_DIR}/${CENTOS_DOCKERFILE_NAME}" $SCRIPT_DIR 
 
-ssh user@clion_docker -p$PORTMOUNT && docker stop "dev-env-centos"
+
+
+echo "starting $ENV_NAME docker container"
+
+docker run -dit --rm -p127.0.0.1:$PORTMOUNT:22 --mount type=bind,source=//$(pwd),destination=//"/home/builder/$CALL_DIR" --name="${CENTOS_ENV_NAME}" $CENTOS_ENV_NAME
+
+ssh developer@clion_docker -p$CENTOS_PORTMOUNT && docker stop "$CENTOS_ENV_NAME"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 
+
 pardir=${PWD##*/}
 pardir=${pardir:-/}
 CALL_DIR=$(echo $pardir | tr '[:upper:]' '[:lower:]')  
@@ -7,4 +8,29 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 grep -qxF "alias alpine-dev-env=\"$SCRIPT_DIR/alpine/build-img.sh\"" ~/.bash_aliases || echo -e "\nalias alpine-dev-env=\"$SCRIPT_DIR/alpine/build-img.sh\"" >> ~/.bash_aliases
 
-grep -qxF "alias centos-dev-env=\"$SCRIPT_DIR/centos/build-img.sh\"" ~/.bash_aliases || echo -e "\nalias centos-dev-env=\"$SCRIPT_DIR/centos/build-img.sh\"" >> ~/.bash_aliases
+
+
+source $SCRIPT_DIR/centos/config
+echo $CENTOS_ENV_NAME
+
+IFS='' read -r -d '' CENTOS_SSHCONFIGENTRY <<EOF
+Host ${CENTOS_ENV_NAME}
+        HostName ${CENTOS_ENV_NAME}
+        StrictHostKeyChecking no
+        UserKnownHostsFile /dev/null
+        GlobalKnownHostsFile /dev/null
+        User ${CENTOS_USERNAME}
+        Port ${CENTOS_PORTMOUNT}
+EOF
+echo "check for ${CENTOS_ENV_NAME} ssh config host entry"
+# Check for dev environment
+grep -xF "Host ${CENTOS_ENV_NAME}" ~/.ssh/config
+
+if [ $? -ne 0 ]; then
+        echo "Writing SSH config host entry for ${CENTOS_ENV_NAME} environment"
+        echo -e "\n$CENTOS_SSHCONFIGENTRY" >> ~/.ssh/config 
+fi
+
+grep -qxF "alias $CENTOS_ENV_NAME=\"$SCRIPT_DIR/centos/build-img.sh\"" ~/.bash_aliases || echo -e "\nalias ${CENTOS_ENV_NAME}=\"$SCRIPT_DIR/centos/build-img.sh\"" >> ~/.bash_aliases
+
+
